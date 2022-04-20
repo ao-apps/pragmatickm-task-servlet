ec4b7d23b1f752885c39ba403ec6edb8ed0da9b1
AO Industries, Inc <support@aoindustries.com>
2022-04-20 04:59:57 -0500

    Significant manual code formatting clean-up…
    … in preparation for an automatic conversion to Google Java Style Guide:
    
    1) Added braces around if/else/for/while where missing
    
    2) Indentation is now 2-space, all file formats
    
      a) HTML indentation changed from tab to 2-space
    
      b) XML indentation changed from 4-space to 2-space
    
    3) Continuation is 4-space for JSON, Java, and JavaScript
    
    4) Updated NetBeans, kwrite, and kate settings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-servlet - Tasks nested within SemanticCMS pages and elements in a Servlet environment.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -21,5 +21,5 @@
  * along with pragmatickm-task-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.pragmatickm.task.servlet {
-	exports com.pragmatickm.task.servlet;
+  exports com.pragmatickm.task.servlet;
 }
